1201f924d5a67947c99d9799516f6c308b5d2a76
claudesimo1990 <45970298+claudesimo1990@users.noreply.github.com>
2021-02-22 18:03:12 +0100

    resolve bug image

--- src/app/Models/Admin/Admin.php
+++ src/app/Models/Admin/Admin.php
@@ -9,12 +9,13 @@ use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
 class Admin extends Authenticatable implements ImagesInterface
 {
-    use HasFactory,Notifiable;
+    use HasFactory, Notifiable;
 
     protected $guarded = [];
 
@@ -25,32 +26,49 @@ class Admin extends Authenticatable implements ImagesInterface
 
     public function initDirectory()
     {
-        //
+        $images = public_path('Admin/uploads/Images');
+        $thumb = public_path('Admin/uploads/Thumbails');
+
+        if (!File::isDirectory($images)) {
+
+            File::makeDirectory($images, 0777, true, true);
+
+        }
+
+        if (!File::isDirectory($thumb)) {
+
+            File::makeDirectory($thumb, 0777, true, true);
+
+        }
+
+
     }
 
     public function createImage(UploadedFile $image, $basename)
     {
-            $original = $basename . '.' . $image->getClientOriginalExtension();
+        $this->initDirectory();
 
-            $thumbmail = $basename . '_thumb.' . $image->getClientOriginalExtension();
+        $original = $basename . '.' . $image->getClientOriginalExtension();
 
-            $img = Image::make($image)->fit(250,250, function ($constraint) {
+        $thumbmail = $basename . '_thumb.' . $image->getClientOriginalExtension();
 
-                $constraint->aspectRatio();
+        $img = Image::make($image)->fit(250, 250, function ($constraint) {
 
-            });
+            $constraint->aspectRatio();
 
-            $img->stream();
+        });
 
-            Storage::disk('uploads')->put('/Admin/Thumbails'. '/' .$thumbmail, $img);
+        $img->stream();
 
-            $image->storeAs('/Admin/Images', $original, 'uploads');
+        Storage::disk('uploads')->put('/Admin/Thumbails' . '/' . $thumbmail, $img);
 
-            // store data into database
-            $this->images()->create([
-                'original' => '/uploads/Admin/Images/'. $original,
-                'thumbail' => '/uploads/Admin/Thumbails/'. $thumbmail
-            ]);
+        $image->storeAs('/Admin/Images', $original, 'uploads');
+
+        // store data into database
+        $this->images()->create([
+            'original' => '/uploads/Admin/Images/' . $original,
+            'thumbail' => '/uploads/Admin/Thumbails/' . $thumbmail
+        ]);
     }
 
     public function deleteImage(ImageUpload $image)
